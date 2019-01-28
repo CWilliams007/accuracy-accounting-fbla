09a3cb021fb60351177aac6a46fc9f255695f81a
Micah Fischer <mfischer981@west-mec.org>
2019-01-28 12:25:37 -0700

    Rebuilt Website From Scratch
    Deleted all files, and restarted website from scratch. Created responsive website design. Add contact form and social media links. Added website content. Created logo and dynamic color scheme. Added fonts and imported content.

--- main.js
+++ main.js
@@ -1,74 +1,15 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.right === "-360px") {
-      x.style.right = "0px";
-    } else {
-      x.style.right = "-360px";
-    }
-
-    var x = document.getElementById("spin");
-    if (x.style.transform === "rotateZ(0deg)") {
-      x.style.transform = "rotateZ(-405deg)";
-    } else {
-      x.style.transform = "rotateZ(0deg)";
-    }
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "280px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
   }
 
- // Hide header below 150px on scroll
-$(window).scroll(function () {
-  if ($(this).scrollTop() < 150) {
-      $('nav').css({
-          'padding': '30px 0px'
-      });
-  }
-});
-// Show header above 50px on scroll
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 50) {
-      $('nav').css({
-          'padding': '10px 0px'
-      });
-  }
-});
 
- // Hide header below 150px on scroll
- $(window).scroll(function () {
-  if ($(this).scrollTop() < 150) {
-      $('div#myDIV').css({
-          'padding': '164px 20px 20px 20px'
-      });
-  }
-});
-// Show header above 50px on scroll
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 50) {
-      $('div#myDIV').css({
-        'padding': '124px 20px 20px 20px'
-      });
-  }
-});
-
-// Search QA Page
-function searchQA() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("h2")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
-
-
-
-
+  
