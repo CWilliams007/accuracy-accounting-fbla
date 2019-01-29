13e03474da849ab6acd961349d1e065d28933605
Micah Fischer <mfischer981@west-mec.org>
2019-01-29 15:59:47 -0700

    DAy 3

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "280px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-  }
+  document.getElementById("mySidenav").style.width = "280px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
-  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
@@ -22,8 +22,3 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
-
-  // Accordion jQuery Ui
-  $( function() {
-    $( "#accordion" ).accordion();
-  } );
